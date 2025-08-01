4a073f7b1d503f9b97996246852be5354ee52e28
Samanyu03 <msamanyu21@gmail.com>
2025-08-01 20:30:21 +0530

    initial commit

---
+++ python operator 1.py
@@ -0,0 +1,9 @@
+tree1=98
+tree2=94
+tree3=41
+tree4=95
+tree5=11
+
+samanyutrees=tree1+tree2+tree3+tree4+tree5
+print("sum of trees is",samanyutrees)
+print("The mean/average of the trees is",samanyutrees/5)
