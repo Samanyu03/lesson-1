9de27e01f01d15e2f0753dd715b0833b1f96e7ea
Samanyu03 <msamanyu21@gmail.com>
2025-08-03 11:43:41 +0530

    initial commit

---
+++ divisibality.py
@@ -0,0 +1,16 @@
+num=float(input("enter a value "))
+if (num%3==0):
+    print("it is divisible by 3")
+else:
+    print("it is not divisible by 3")
+    
+    
+    
+#admin password
+pw=input("enter ur password ")
+admin="1234"
+if (pw==admin):
+    print("login successful!!!")
+    
+else:
+    print("invalid password")
