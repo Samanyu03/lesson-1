39d8c495b402c5c18ed4a5188dd39b54c09acfab
Samanyu03 <msamanyu21@gmail.com>
2025-08-03 11:24:46 +0530

    initial commit

---
+++ profit and loss.py
@@ -0,0 +1,9 @@
+cp=int(input("enter the cost of the product "))
+sp=int(input("enter the sales amount for the product "))
+
+if (sp>cp):
+    print("you have made a profit of ", sp-cp)
+    
+else:
+    print("you made a undefying loss of ",cp-sp)
+    
