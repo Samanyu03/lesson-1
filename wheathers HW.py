ee0540cd59fa473d55443ca2bd3ec4a2fd385198
Samanyu03 <msamanyu21@gmail.com>
2025-08-03 21:18:48 +0530

    initial commit

---
+++ wheathers HW.py
@@ -0,0 +1,9 @@
+whea=input("whats ur wheather near ur area?? ")
+if (whea=sunny):
+    print("wear some cotten clothes")
+    
+elif (whea="rainy"):
+    print("stay indoors and wear warm clothes")
+    
+else:
+    print("wear some woolen clothes")
