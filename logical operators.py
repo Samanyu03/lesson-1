02eafb53aff11ee9f30077830e4456d925e4c7ac
Samanyu03 <msamanyu21@gmail.com>
2025-08-04 20:34:10 +0530

    initial commit

---
+++ logical operators.py
@@ -0,0 +1,21 @@
+a=1
+b=2
+c=-4
+
+if a>0 and b>0 and c>0:
+    print("all the num are greater than 0")
+    
+else :
+    print("one of the num is not greater than 0")
+    
+
+## OR operator
+a=6
+c=-8
+
+if a>0 or c>0:
+    print("either of the num are greater than 0") 
+    
+else:
+    print("none of them are greater than 0")
+    
