879b2f2ca4d572d06aa7ec507aae5a32cf690e2e
Samanyu03 <msamanyu21@gmail.com>
2025-08-08 20:37:29 +0530

    initial commit

---
+++ indentity operators.py
@@ -0,0 +1,16 @@
+x=456.9
+if(type(x)==int):
+    print("True")
+    
+else:
+    print("False")
+    
+a=60
+b=60
+c=70
+
+if (a is b):
+    print("they have same identity")
+    
+if (a is not c):
+    print("they have different identity")
