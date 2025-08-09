d54725300fe7597cb3659e88b2a933cdd960ad7c
Samanyu03 <msamanyu21@gmail.com>
2025-08-09 12:32:30 +0530

    initial commit

---
+++ operator.py
@@ -0,0 +1,16 @@
+s=4
+a=5
+m=6
+A=2
+n=9
+y=8
+u=a+s-n*y
+print("the value of a+s-n*y ", u)
+
+name="s"
+age=9
+if(name=="sam" or name=="john" and age>=10):
+    print("hello!welcome!!")
+    
+else:
+    print("goodbyegoodbye👋")
