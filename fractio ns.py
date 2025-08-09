862daef183a4ffb3f2b0016e4e62d526564d3fa4
Samanyu03 <msamanyu21@gmail.com>
2025-08-09 12:39:29 +0530

    initial commit

---
+++ fractio ns.py
@@ -0,0 +1,8 @@
+a=int(input("enter a denominator"))
+b=int(input("enter a numerator"))
+
+if(b%a==0):
+    print("numerator is divisible by denominator")
+    
+else:
+    print("numerator is not divisible by denominator")
