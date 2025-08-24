e46dbce718565beee949ac665a30e30dbd3c7394
Samanyu03 <msamanyu21@gmail.com>
2025-08-24 11:37:25 +0530

    initial  commit

---
+++ triangle pattern.py
@@ -0,0 +1,13 @@
+a=int(input("Enter the number of rows: "))
+
+for i in range(a):
+    for j in range(i+1):
+        print("* ",end=" ")
+        
+    print()
+        
+        
+        
+b=3
+for g in range(b):
+    print(g)
